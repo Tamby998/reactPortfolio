e1a0efea92ff7b8371b0f1211247c9f6355302c3
therihar@etceterum <therihar@etceterum.net>
2021-08-21 13:44:45 +0300

    Correction d'erreur

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./App.css";
 import Main from "./containers/Main";
 import { ThemeProvider } from "styled-components";
 import { chosenTheme } from "./theme";
 import { GlobalStyles } from "./global";
 
-import ReactGA from "react-ga";
 function App() {
   return (
     <ThemeProvider theme={chosenTheme}>
